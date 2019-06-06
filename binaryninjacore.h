014d248cd891e0139328d02778d49984d5b0d668
Glenn Smith <couleeapps@gmail.com>
2019-06-05 21:30:04 -0400

    Tag TextToken Type

--- binaryninjacore.h
+++ binaryninjacore.h
@@ -240,6 +240,7 @@ extern "C"
 		FieldNameToken = 21,
 		NameSpaceToken = 22,
 		NameSpaceSeparatorToken = 23,
+		TagToken = 24,
 		// The following are output by the analysis system automatically, these should
 		// not be used directly by the architecture plugins
 		CodeSymbolToken = 64,
